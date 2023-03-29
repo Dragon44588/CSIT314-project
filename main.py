199c81e9c44ec8f3fcd1ea0d1fd3c7ffdc1c2e3d
Dragon44588 <105646122+Dragon44588@users.noreply.github.com>
2023-03-29 17:10:30 +1100

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+spacer = "-"*50
+
+
+def index():
+    print(spacer)
+    print("Only Tradies")
+    print(spacer)
+    print("Please select one of the options below")
+    print("User login (U)")
+    print("Tradie login(T)")
+    print(spacer)
+    selection = input("Enter a selection: ")
+
+    if selection.lower() == "u":
+        loginUser()
+    elif selection.lower() == "t":
+        loginTrade()
+    else:
+        print("please enter U or T")
+
+def loginUser():
+    print(spacer)
+    print("User login")
+    print(spacer)
+
+
+def loginTrade():
+    print(spacer)
+    print("Tradie login")
+    print(spacer)
+
+if __name__ == "__main__":
+    index()
