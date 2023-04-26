2e2566a88c1dc3c4be5522b6fb5289a3923c1f60
Dragon44588 <adrianvergan@outlook.com>
2023-04-26 18:35:44 +1000

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ spacer = "-"*50
 
 def index():
 
+    ##testing data
+    #default user and tradie for testing purposes
+    testUser = Client(1, 100, False, 10, 20)
+    testTradie = Professional(2, 0, False, 20, 50, 0, 0, 0, 0, 0, 0) 
+
     while True:
 
         print(spacer)
