4292434ac579a4c3014404e6b6cf135d5db96579
Dragon44588 <105646122+Dragon44588@users.noreply.github.com>
2023-05-03 17:21:23 +1000

    created the generator file

--- client.py
+++ client.py
@@ -1,12 +1,23 @@
 from users import User
 
 class Client(User):
-    pass
+    def __init__(self, id, balance, subscriber, xcoord, ycoord):
+        self.id = id
+        self.balance = balance
+        self.subscriber = subscriber
+        self.xcoord = xcoord
+        self.ycoord = ycoord
+        self.jobs = []
+
+    
 
     def subscribe(self):
         print("Currently a subscriber?: {}".format(self.subStatus))
         
     def payOnDemand(self):
         print("pay on demand")
+
+    def appendJob(self, job):
+        self.jobs.append(job)
         
     
---
+++ generator.py
@@ -0,0 +1,50 @@
+#require 20 tradies(proffessionals) and 20 clients
+
+#User variables
+#
+#ID
+#Balance
+#Subscriber
+#Xcoord
+#Ycoord
+#jobs
+#
+#Professionals variables
+#
+#ID
+#Balance
+#Subscriber
+#Xcoord
+#Ycoord
+#nextEvent
+#skill
+#reliability
+#speed
+#rating
+#ammountRated
+
+userNames = ['Liam', 'Noah', 'Oliver', 'Elijah','James','William','Benjamin','Lucas','Henry','Theodore','Jack','Levi',
+         'Alexander','Jackson','Mateo','Daniel','Michael','Mason','Sebastian','Ethan','Logan','Owen','Samuel','Jacob',
+         'Asher','Aiden','John','Joseph','Wyatt','David','Leo','Luke','Julian','Hudson','Grayson','Matthew','Ezra',
+         'Gabriel','Carter','Isaac','Jayden','Luca','Anthony','Dylan','Lincoln','Thomas','Maverick','Elias','Josiah',
+         'Charles','Caleb','Christopher','Ezekiel','Miles','Jaxon','Isaiah','Andrew','Joshua','Nathan','Nolan','Adriana',
+         'Cameron','Santiago','Eli','Aaron','Ryan','Angel','Cooper','Waylon','Easton','Kai','Christian','Landon','Colton',
+         'Roman','Axel','Brooks','Jonathan','Robert','Jameson','Ian','Everett','Greyson','Wesley','Jeremiah','Hunter',
+         'Leonardo','Jordan','Jose','Bennett','Silas','Nicholas','Parker','Beau','Weston','Austin','Connor','Carson',
+         'Dominic','Xavier']
+
+buisnessNames = ['concrete', 'brickies', 'carpentry', 'lumber', 'jims construction', 'trades']
+buisnessTags = ['Inc', 'PTY LTD', 'LTD', 'Limited']
+
+def generator(numUsers, numPros, numEvents):
+    for i in range(0, numUsers):
+        print(i)
+
+
+def generateUser():
+    print("gen")
+
+def generateProfessionals():
+    print("gen")
+    
+generator(10, 0, 0)
